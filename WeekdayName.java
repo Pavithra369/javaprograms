cc297c7746560c6feb987d9db0b0364dc06f5993
Pavithra369 <127390139+Pavithra369@users.noreply.github.com>
2023-03-09 15:03:47 +0530

    Add files via upload

---
+++ WeekdayName.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class WeekdayName {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number between 1 and 7: ");
+        int num = input.nextInt();
+
+        String weekday;
+
+        switch (num) {
+            case 1:
+                weekday = "Sunday";
+                break;
+            case 2:
+                weekday = "Monday";
+                break;
+            case 3:
+                weekday = "Tuesday";
+                break;
+            case 4:
+                weekday = "Wednesday";
+                break;
+            case 5:
+                weekday = "Thursday";
+                break;
+            case 6:
+                weekday = "Friday";
+                break;
+            case 7:
+                weekday = "Saturday";
+                break;
+            default:
+                weekday = "Invalid input";
+                break;
+        }
+
+        System.out.println("The weekday is " + weekday);
+    }
+}
