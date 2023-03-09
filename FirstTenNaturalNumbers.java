32ddae9214944b08135bd6ce297871536ec9eed2
Pavithra369 <127390139+Pavithra369@users.noreply.github.com>
2023-03-09 15:31:12 +0530

    output added

--- FirstTenNaturalNumbers.java
+++ FirstTenNaturalNumbers.java
@@ -1,7 +1,18 @@
-public class FirstTenNaturalNumbers {
+//public class FirstTenNaturalNumbers {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
     }
 }
+//output:
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
